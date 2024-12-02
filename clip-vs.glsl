e652ca545f4001cce51d9b05358d597b3a014a80
catarinagc <catarinagcostaa@gmail.com>
2024-12-02 01:31:15 +0000

    shapes code simplification bugged, random color functionality working

--- clip-vs.glsl
+++ clip-vs.glsl
@@ -3,10 +3,22 @@
 in vec4 inPosition;
 in vec4 inColor;
 out vec4 exColor;
+uniform int GroupID;
 
 uniform mat4 Matrix;
 
+float random(vec3 seed) {
+    return fract(sin(dot(seed, vec3(12.9898, 78.233, 45.164))) * 43758.5453123);
+}
+
 void main(void) {
     gl_Position = Matrix * inPosition;
-    exColor = inColor;
+  //  exColor = inColor;
+
+    exColor = vec4(
+        random(GroupID + vec3(0.0)),
+        random(GroupID + vec3(1.0)),
+        random(GroupID + vec3(2.0)),
+        1.0f
+    );
 }
--- hello-2d-world.cpp
+++ hello-2d-world.cpp
@@ -31,6 +31,7 @@ class MyApp : public mgl::App {
   GLuint VaoId, VboId[2];
   std::unique_ptr<mgl::ShaderProgram> Shaders;
   GLint MatrixId;
+  GLint GroupID;
 
   void createShaderProgram();
   void createBufferObjects();
@@ -48,10 +49,12 @@ void MyApp::createShaderProgram() {
   Shaders->addAttribute(mgl::POSITION_ATTRIBUTE, POSITION);
   Shaders->addAttribute(mgl::COLOR_ATTRIBUTE, COLOR);
   Shaders->addUniform("Matrix");
+  Shaders->addUniform("GroupID");
 
   Shaders->create();
 
   MatrixId = Shaders->Uniforms["Matrix"].index;
+  GroupID = Shaders->Uniforms["GroupID"].index;
 }
 
 //////////////////////////////////////////////////////////////////// VAOs & VBOs
@@ -73,23 +76,23 @@ const Vertex Vertices[] = {
     {{0.50f, 0.75f, 0.0f, 1.0f}, {0.0f, 0.0f, 1.0f, 1.0f}},
 */
     //right triangle
-    {{0.0f, 0.0f, 0.0f, 1.0f}, {1.0f, 0.0f, 0.0f, 1.0f}},
-    {{0.25f, 0.0f, 0.0f, 1.0f}, {0.0f, 1.0f, 0.0f, 1.0f}},
-    {{0.0f, 0.25f, 0.0f, 1.0f}, {0.0f, 0.0f, 1.0f, 1.0f}},
+    {{0.0f, 0.0f, 0.0f, 1.0f}, {1.0f, 1.0f, 1.0f, 1.0f}},
+    {{0.25f, 0.0f, 0.0f, 1.0f}, {1.0f, 1.0f, 1.0f, 1.0f}},
+    {{0.0f, 0.25f, 0.0f, 1.0f}, {1.0f, 1.0f, 1.0f, 1.0f}},
 
     //square
-    {{0.0f, 0.0f, 0.0f, 1.0f}, {1.0f, 0.0f, 0.0f, 1.0f}},
-    {{0.25f, 0.0f, 0.0f, 1.0f}, {1.0f, 0.0f, 0.0f, 1.0f}},
-    {{0.25f, 0.25f, 0.0f, 1.0f}, {1.0f, 0.0f, 0.0f, 1.0f}},
-    {{0.0f, 0.25f, 0.0f, 1.0f}, {1.0f, 0.0f, 0.0f, 1.0f}},
+    {{0.0f, 0.0f, 0.0f, 1.0f}, {1.0f, 1.0f, 1.0f, 1.0f}},
+    {{0.25f, 0.0f, 0.0f, 1.0f}, {1.0f, 1.0f, 1.0f, 1.0f}},
+    {{0.25f, 0.25f, 0.0f, 1.0f}, {1.0f, 1.0f, 1.0f, 1.0f}},
+    {{0.0f, 0.25f, 0.0f, 1.0f}, {1.0f, 1.0f, 1.0f, 1.0f}},
 
     //parallelogram
-    {{0.0f, 0.25f, 0.0f, 1.0f}, {1.0f, 0.0f, 0.0f, 1.0f}},
-    {{0.25f, 0.25f, 0.0f, 1.0f}, {1.0f, 0.0f, 0.0f, 1.0f}},
-    {{0.5f, 0.25f, 0.0f, 1.0f}, {1.0f, 0.0f, 0.0f, 1.0f}},
-    {{0.25f, 0.0f, 0.0f, 1.0f}, {1.0f, 0.0f, 0.0f, 1.0f}},
-    {{0.5f, 0.0f, 0.0f, 1.0f}, {1.0f, 0.0f, 0.0f, 1.0f}},
-    {{0.75f, 0.0f, 0.0f, 1.0f}, {1.0f, 0.0f, 0.0f, 1.0f}}
+    {{0.0f, 0.25f, 0.0f, 1.0f}, {1.0f, 1.0f, 1.0f, 1.0f}},
+    {{0.25f, 0.25f, 0.0f, 1.0f}, {1.0f, 1.0f, 1.0f, 1.0f}},
+    {{0.5f, 0.25f, 0.0f, 1.0f}, {1.0f, 1.0f, 1.0f, 1.0f}},
+    {{0.25f, 0.0f, 0.0f, 1.0f}, {1.0f, 1.0f, 1.0f, 1.0f}},
+    {{0.5f, 0.0f, 0.0f, 1.0f}, {1.0f, 1.0f, 1.0f, 1.0f}},
+    {{0.75f, 0.0f, 0.0f, 1.0f}, {1.0f, 1.0f, 1.0f, 1.0f}}
 };
 
 const GLubyte Indices[] = {0,1,2,
@@ -151,107 +154,85 @@ void MyApp::destroyBufferObjects() {
 const glm::mat4 I(1.0f);
 const glm::mat4 M = glm::translate(glm::vec3(-1.0f, -1.0f, 0.0f));
 const glm::mat4 L = glm::translate(glm::vec3(-1.0f, 0.0f, 0.0f));
-const glm::mat4 M1 = glm::translate(glm::vec3(-0.25f, 0.0f, 0.0f));
+const glm::mat4 M1 = glm::translate(glm::vec3(0.0f, -0.5f, 0.0f));
 glm::mat4 R = glm::rotate(I, glm::radians(90.0f), glm::vec3(0.0f, 0.0f, 1.0f));
-//M1 = glm::rotate(M1, glm::vec3(0.0f,-1.0f,0.0f));
 glm::mat4 Transform = R * M1;
 const glm::mat4 C = glm::translate(glm::vec3(0.0f, 0.25f, 0.0f));
 const glm::mat4 C2 = glm::translate(glm::vec3(0.25f, 0.0f, 0.0f));
 const glm::mat4 S = glm::scale(I, glm::vec3(2.0f, 2.0f, 1.0f));
 const glm::mat4 C3 = glm::translate(glm::vec3(-0.25f, 0.0f, 0.0f));
+const glm::mat4 Ry = glm::rotate(I, glm::radians(45.0f), glm::vec3(0.0f, 0.0f, 1.0f));
+
+/*
+glm::vec4 randColor() {
+    float randNum = rand() / RAND_MAX;
+    return glm::vec4(randNum,randNum,randNum,1.0f);
+}
+*/
+/*void updateColor(int startVertice, int endVertice, glm::vec4 color) {
+    for (int i = startVertice; i < endVertice; i++) {
+        Vertices[i].RGBA= color;
+    }
+} */
+/*
+void updateVertexColors(GLuint vbo, int vertexCount, const Vertex* vertices, glm::vec4 color) {
+    // Create a temporary array to hold updated vertices
+    Vertex updatedVertices[];
+    for (int i = 0; i < vertexCount; ++i) {
+        updatedVertices[i] = vertices[i];
+        // Update the color for each vertex
+        updatedVertices[i].RGBA[0] = color.r;
+        updatedVertices[i].RGBA[1] = color.g;
+        updatedVertices[i].RGBA[2] = color.b;
+        updatedVertices[i].RGBA[3] = color.a;
+    }
+
+    // Bind the buffer and update with the modified vertex data
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glBufferSubData(GL_ARRAY_BUFFER, 0, vertexCount * sizeof(Vertex), updatedVertices.data());
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+} */
+
+void drawSmallTriangle(GLint MatrixId, glm::mat4 rotationMatrix, glm::mat4 scaleMatrix, glm::mat4 translationMatrix, GLint GroupID) {
+    glUniformMatrix4fv(MatrixId, 1, GL_FALSE, glm::value_ptr(translationMatrix*rotationMatrix*scaleMatrix));
+    glUniform1i(GroupID, 1);
+    glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_BYTE,   //3 = n de vertices
+        reinterpret_cast<GLvoid*>(0));
+}
+
+void drawSquare(GLint MatrixId, glm::mat4 rotationMatrix, glm::mat4 scaleMatrix, glm::mat4 translationMatrix) {
+    glUniformMatrix4fv(MatrixId, 1, GL_FALSE, glm::value_ptr(translationMatrix*  rotationMatrix * scaleMatrix));
+    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_BYTE, reinterpret_cast<GLvoid*>(3 * sizeof(Indices[0])));
+}
+
+void drawMediumTriangle(GLint MatrixId, glm::mat4 rotationMatrix, glm::mat4 scaleMatrix, glm::mat4 translationMatrix) {
+    //drawSmallTriangle(MatrixId,rotationMatrix,scaleMatrix,C*translationMatrix);
+    drawSquare(MatrixId, rotationMatrix, scaleMatrix, translationMatrix);
+    //drawSmallTriangle(MatrixId, rotationMatrix, scaleMatrix, C2*translationMatrix);
+}
+
+void drawBigTriangle(GLint MatrixId, glm::mat4 rotationMatrix, glm::mat4 scaleMatrix, glm::mat4 translationMatrix) {
+    drawMediumTriangle(MatrixId,rotationMatrix,scaleMatrix, rotationMatrix);
+    drawMediumTriangle(MatrixId, rotationMatrix, R*scaleMatrix, rotationMatrix);
+}
+
+void drawParallelogram(GLint MatrixId, glm::mat4 rotationMatrix, glm::mat4 scaleMatrix, glm::mat4 translationMatrix) {
+    glUniformMatrix4fv(MatrixId, 1, GL_FALSE, glm::value_ptr(translationMatrix*rotationMatrix * scaleMatrix));
+    glDrawElements(GL_TRIANGLES, 12, GL_UNSIGNED_BYTE, reinterpret_cast<GLvoid*>(9 * sizeof(Indices[0])));
+}
+
 void MyApp::drawScene() { //onde defino a matriz de transformação e o que vou desenhar
   // Drawing directly in clip space
 
   glBindVertexArray(VaoId);
   Shaders->bind();
   
+  drawSmallTriangle(MatrixId, I, S, I, GroupID);
+  //drawMediumTriangle(MatrixId, I, I, I);
+  //drawBigTriangle(MatrixId, I, I, I);
+  //drawParallelogram(MatrixId, I, I, I);
+  //drawSquare(MatrixId,I,I,I);
 
-  /*
-  glUniformMatrix4fv(MatrixId, 1, GL_FALSE, glm::value_ptr(I));
-  glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_BYTE,   //3 = n de vertices
-                 reinterpret_cast<GLvoid *>(0));
-
-  glUniformMatrix4fv(MatrixId, 1, GL_FALSE, glm::value_ptr(M));
-  glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_BYTE, reinterpret_cast<GLvoid*>(3*sizeof(Indices[0]))); //6 pq sao "2 triangulos"
-  
-  glUniformMatrix4fv(MatrixId, 1, GL_FALSE, glm::value_ptr(L));
-  glDrawElements(GL_TRIANGLES, 12, GL_UNSIGNED_BYTE, reinterpret_cast<GLvoid*>(9 * sizeof(Indices[0])));
-  */
-
-  //small triangle 1
-  /*
-  glUniformMatrix4fv(MatrixId, 1, GL_FALSE, glm::value_ptr(I));
-  glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_BYTE,  
-      reinterpret_cast<GLvoid*>(0)); 
-  glUniformMatrix4fv(MatrixId, 1, GL_FALSE, glm::value_ptr(R));
-  glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_BYTE, 
-      reinterpret_cast<GLvoid*>(0));
-*/
-  //small triangle 2
-  /*
-  glUniformMatrix4fv(MatrixId, 1, GL_FALSE, glm::value_ptr(I));
-  glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_BYTE,
-      reinterpret_cast<GLvoid*>(0));
-  glUniformMatrix4fv(MatrixId, 1, GL_FALSE, glm::value_ptr(R));
-  glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_BYTE,
-      reinterpret_cast<GLvoid*>(0));
- */
-
-  //medium triangle
-  /*
-  glUniformMatrix4fv(MatrixId, 1, GL_FALSE, glm::value_ptr(C));
-  glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_BYTE,
-      reinterpret_cast<GLvoid*>(0));
-  glUniformMatrix4fv(MatrixId, 1, GL_FALSE, glm::value_ptr(C2));
-  glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_BYTE,
-      reinterpret_cast<GLvoid*>(0));
-  glUniformMatrix4fv(MatrixId, 1, GL_FALSE, glm::value_ptr(I));
-  glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_BYTE, reinterpret_cast<GLvoid*>(3 * sizeof(Indices[0])));
-  */
-  //big triangle 1 (2 medium triangles)
-  glUniformMatrix4fv(MatrixId, 1, GL_FALSE, glm::value_ptr(C));
-  glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_BYTE,
-      reinterpret_cast<GLvoid*>(0));
-  glUniformMatrix4fv(MatrixId, 1, GL_FALSE, glm::value_ptr(C2));
-  glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_BYTE,
-      reinterpret_cast<GLvoid*>(0));
-  glUniformMatrix4fv(MatrixId, 1, GL_FALSE, glm::value_ptr(I));
-  glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_BYTE, reinterpret_cast<GLvoid*>(3 * sizeof(Indices[0])));
-
-  glUniformMatrix4fv(MatrixId, 1, GL_FALSE, glm::value_ptr(C*R));
-  glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_BYTE,
-      reinterpret_cast<GLvoid*>(0));
-  glUniformMatrix4fv(MatrixId, 1, GL_FALSE, glm::value_ptr(R));
-  glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_BYTE, reinterpret_cast<GLvoid*>(3 * sizeof(Indices[0])));
-  glUniformMatrix4fv(MatrixId, 1, GL_FALSE, glm::value_ptr(C3*R));
-  glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_BYTE,
-      reinterpret_cast<GLvoid*>(0));
-
-  //big triangle 2 (2 medium triangles)
-  /*
-  glUniformMatrix4fv(MatrixId, 1, GL_FALSE, glm::value_ptr(C));
-  glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_BYTE,
-      reinterpret_cast<GLvoid*>(0));
-  glUniformMatrix4fv(MatrixId, 1, GL_FALSE, glm::value_ptr(C2));
-  glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_BYTE,
-      reinterpret_cast<GLvoid*>(0));
-  glUniformMatrix4fv(MatrixId, 1, GL_FALSE, glm::value_ptr(I));
-  glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_BYTE, reinterpret_cast<GLvoid*>(3 * sizeof(Indices[0])));
-
-  glUniformMatrix4fv(MatrixId, 1, GL_FALSE, glm::value_ptr(C * R));
-  glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_BYTE,
-      reinterpret_cast<GLvoid*>(0));
-  glUniformMatrix4fv(MatrixId, 1, GL_FALSE, glm::value_ptr(R));
-  glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_BYTE, reinterpret_cast<GLvoid*>(3 * sizeof(Indices[0])));
-  glUniformMatrix4fv(MatrixId, 1, GL_FALSE, glm::value_ptr(C3 * R));
-  glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_BYTE,
-      reinterpret_cast<GLvoid*>(0));
-*/
-  /*
-  glUniformMatrix4fv(MatrixId, 1, GL_FALSE, glm::value_ptr(I));
-  glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_BYTE,   //3 = n de vertices
-      reinterpret_cast<GLvoid*>(0));
-*/
   Shaders->unbind();
   glBindVertexArray(0);
 }
